486e816dd2b5a7e53e3756bda0761ee48ffb08bf
Alexey Cherevko <you@example.com>
2020-03-19 21:57:19 +0700

    2155 190320

--- js/application.js
+++ js/application.js
@@ -1,36 +1,57 @@
-// our application constructor
+﻿// our application constructor
 function application () {
 }
 
+application.prototype.displayErrorMessage = function(message) {
+	$('#deal-list').html(message);
+	$('#deal-sum').html(message);
+}
+
 application.prototype.displayCurrentUser = function(selector) {
+	BX24.callMethod('user.current', {}, function(result){
+		$(selector).html(result.data().NAME + ' ' + result.data().LAST_NAME);
+	});
+}
 
- //$(selector).html('Hello !');
+application.prototype.displayUserClosedDeals = function (idUser) {
 
-let url1="https://b24-fik4sy.bitrix24.ru/rest/1/89mi95d2zftrbhvr/user.current";
-$.ajax({
-    url: url1,
-    method: "GET",
-    //contentType: "",
-    //data:{order:{'STAGE_TITLE':'ASC'},select:['ID','TITLE'],filter:{'<ID':20, '>=ID':5}},
-	//data:{select:['EMAIL']},
-
-    success: function (data) {
-        let result=data.result;
-        console.log(result);
-        //console.log(data);
-	    //$(selector).html('Hello ' + result.data().NAME + ' ' + result.data().LAST_NAME + '!');
-	    $(selector).html('Hello '+result.EMAIL+
-	    	' '+result['ID']);
-     }
-});
-
-/*	BX24.callMethod(
-        'user.current',
-        {},
-        function(result){
-		    $(selector).html('Hello ' + result.data().NAME + ' ' + result.data().LAST_NAME + '!');
-	    }
-    );*/
+	var dealSum = 0, dealHTML = '';
+	
+	var curapp = this;
+	
+	BX24.callMethod(
+		"crm.deal.list", 
+		{ 
+			order: { "DATE_CREATE": "ASC" },
+			filter: { "ASSIGNED_BY_ID": idUser, "CLOSED": 'Y' },
+			select: [ "ID", "TITLE", "OPPORTUNITY"]
+		}, 
+		function(result) 
+		{
+			if (result.error()) {
+                curapp.displayErrorMessage('К сожалению, произошла ошибка получения сделок. Попробуйте повторить отчет позже');
+				console.error(result.error());
+			}
+			else
+			{
+				var data = result.data();
+				
+				for (indexDeal in data) {
+					dealSum += parseFloat(data[indexDeal].OPPORTUNITY);
+					dealHTML += '<tr><th scope="row">' + data[indexDeal].ID + '</th><td>' + data[indexDeal].TITLE +'</td><td>'
+						+ data[indexDeal].OPPORTUNITY + '</td></tr>';
+				}
+							
+				if (result.more())
+					result.next();
+				else {
+					$('#deal-list').html(dealHTML);
+					$('#deal-sum').html('<span class="volume">' + dealSum + '</span><br/>общая сумма');						
+				}
+					
+			}
+		}
+	);
 }
 
 // create our application
