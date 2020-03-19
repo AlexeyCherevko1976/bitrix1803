36f756769307462b33c1ffcc5965ac0965700723
Alexey Cherevko <you@example.com>
2020-03-19 21:31:11 +0700

    23-30 190320

--- js/application.js
+++ js/application.js
@@ -3,13 +3,34 @@ function application () {
 }
 
 application.prototype.displayCurrentUser = function(selector) {
-	BX24.callMethod(
+
+ //$(selector).html('Hello !');
+
+let url1="https://b24-fik4sy.bitrix24.ru/rest/1/89mi95d2zftrbhvr/user.current";
+$.ajax({
+    url: url1,
+    method: "GET",
+    //contentType: "",
+    //data:{order:{'STAGE_TITLE':'ASC'},select:['ID','TITLE'],filter:{'<ID':20, '>=ID':5}},
+	//data:{select:['EMAIL']},
+
+    success: function (data) {
+        let result=data.result;
+        console.log(result);
+        //console.log(data);
+	    //$(selector).html('Hello ' + result.data().NAME + ' ' + result.data().LAST_NAME + '!');
+	    $(selector).html('Hello '+result.EMAIL+
+	    	' '+result['ID']);
+     }
+});
+
+/*	BX24.callMethod(
         'user.current',
         {},
         function(result){
 		    $(selector).html('Hello ' + result.data().NAME + ' ' + result.data().LAST_NAME + '!');
 	    }
-    );
+    );*/
 }
 
 // create our application
