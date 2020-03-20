a18f117f8055b3e2c03ec073bb28b834e3de515d
Alexey Cherevko <you@example.com>
2020-03-20 20:02:26 +0700

    20-02 200320

--- js/application.js
+++ js/application.js
@@ -5,16 +5,10 @@ function application () {
 
 application.prototype.displayCurrentUser = function(selector) {
 
- //$(selector).html('Hello !');
-
 let url1="https://b24-fik4sy.bitrix24.ru/rest/1/89mi95d2zftrbhvr/user.current";
 $.ajax({
     url: url1,
     method: "GET",
-    //contentType: "",
-    //data:{order:{'STAGE_TITLE':'ASC'},select:['ID','TITLE'],filter:{'<ID':20, '>=ID':5}},
-	//data:{select:['EMAIL']},
-
     success: function (data) {
         console.log(data);
         let result=data.result;
