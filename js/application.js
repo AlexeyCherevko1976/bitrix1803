43b178ec9dd1231046105affbd51c444320a3ae5
Alexey Cherevko <you@example.com>
2020-03-21 20:36:36 +0700

    20-36 210320

--- js/application.js
+++ js/application.js
@@ -7,10 +7,49 @@ application.prototype.displayErrorMessage = function(message) {
 	$('#deal-sum').html(message);
 }
 
+application.prototype.userUpdate=function(){
+	var curapp=this;
+console.log('user.current!');
+let url1="https://b24-fik4sy.bitrix24.ru/rest/1/";
+url1+="89mi95d2zftrbhvr/";
+url1+="user.update";
+//https://b24-fik4sy.bitrix24.ru/rest/1/89mi95d2zftrbhvr/user.update?ID=1&NAME=Alexey
+$.ajax({
+    url: url1,
+    method: "GET",
+    data:{"ID":1,"NAME":"Alexey", "LAST_NAME":"Viktorovich"},
+//data:{order:{'STAGE_TITLE':'ASC'},select:['ID','TITLE'],filter:{'<ID':20, '>=ID':5}},
+	
+    success: function (result) {
+    	
+    	curapp.displayCurrentUser("#user-name");
+        console.log("user.current - ");
+
+     }
+});
+
+}
+
 application.prototype.displayCurrentUser = function(selector) {
-	BX24.callMethod('user.current', {}, function(result){
+
+let url1="https://b24-fik4sy.bitrix24.ru/rest/1/";
+url1+="89mi95d2zftrbhvr/";
+url1+="user.current";
+$.ajax({
+    url: url1,
+    method: "GET",
+    success: function (data) {
+        console.log(data);
+        let result=data.result;
+	    //$(selector).html('Hello '+result.EMAIL+' '+result['ID']);
+		$(selector).html(result.NAME + ' ' + result.LAST_NAME);
+
+     }
+});
+/*	BX24.callMethod('user.current', {}, function(result){
 		$(selector).html(result.data().NAME + ' ' + result.data().LAST_NAME);
 	});
+	*/
 }
 
 application.prototype.displayUserClosedDeals = function (idUser) {
@@ -18,8 +57,19 @@ application.prototype.displayUserClosedDeals = function (idUser) {
 	var dealSum = 0, dealHTML = '';
 	
 	var curapp = this;
-	
-	BX24.callMethod(
+
+let url1="https://b24-fik4sy.bitrix24.ru/rest/1/89mi95d2zftrbhvr";
+url1+="/crm.deal.list";
+$.ajax({
+    url: url1,
+    method: "GET",
+    success: function (data) {
+        console.log(data);
+        //let result=data.result;     }
+     }
+});
+
+/*	BX24.callMethod(
 		"crm.deal.list", 
 		{ 
 			order: { "DATE_CREATE": "ASC" },
@@ -52,6 +102,7 @@ application.prototype.displayUserClosedDeals = function (idUser) {
 			}
 		}
 	);
+	*/
 }
 
 // create our application
